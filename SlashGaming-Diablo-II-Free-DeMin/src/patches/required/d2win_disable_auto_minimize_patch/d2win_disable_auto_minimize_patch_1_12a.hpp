43460362fb4a4c6a12a1c462e956a05162fc7529
IAmTrial <IAmRealTrial@gmail.com>
2020-01-05 00:34:01 -0800

    Port DeMin patch to 1.12A

--- SlashGaming-Diablo-II-Free-DeMin/src/patches/required/d2win_disable_auto_minimize_patch/d2win_disable_auto_minimize_patch_1_12a.hpp
+++ SlashGaming-Diablo-II-Free-DeMin/src/patches/required/d2win_disable_auto_minimize_patch/d2win_disable_auto_minimize_patch_1_12a.hpp
@@ -43,46 +43,17 @@
  *  work.
  */
 
-#include "d2win_disable_auto_minimize_patch.hpp"
+#ifndef SGD2FDMN_PATCHES_REQUIRED_D2WIN_DISABLE_AUTO_MINIMIZE_PATCH_D2WIN_DISABLE_AUTO_MINIMIZE_1_12A_HPP_
+#define SGD2FDMN_PATCHES_REQUIRED_D2WIN_DISABLE_AUTO_MINIMIZE_PATCH_D2WIN_DISABLE_AUTO_MINIMIZE_1_12A_HPP_
 
-#include "d2win_disable_auto_minimize_patch_1_00.hpp"
-#include "d2win_disable_auto_minimize_patch_1_03.hpp"
-#include "d2win_disable_auto_minimize_patch_1_05b.hpp"
-#include "d2win_disable_auto_minimize_patch_1_09d.hpp"
-#include "d2win_disable_auto_minimize_patch_1_10.hpp"
-#include "d2win_disable_auto_minimize_patch_1_12a.hpp"
+#include <vector>
+
+#include <sgd2mapi.hpp>
 
 namespace sgd2fdmn::patches {
 
-std::vector<mapi::GamePatch> Make_D2Win_DisableAutoMinimizePatch() {
-  d2::GameVersion running_game_version_id = d2::GetRunningGameVersionId();
+std::vector<mapi::GamePatch> Make_D2Win_DisableAutoMinimizePatch_1_12A();
 
-  switch (running_game_version_id) {
-    case d2::GameVersion::k1_00: {
-      return Make_D2Win_DisableAutoMinimizePatch_1_00();
-    }
+} // namespace SGD2FDMN::patches
 
-    case d2::GameVersion::k1_03: {
-      return Make_D2Win_DisableAutoMinimizePatch_1_03();
-    }
-
-    case d2::GameVersion::k1_05B: {
-      return Make_D2Win_DisableAutoMinimizePatch_1_05B();
-    }
-
-    case d2::GameVersion::k1_09D: {
-      return Make_D2Win_DisableAutoMinimizePatch_1_09D();
-    }
-
-    case d2::GameVersion::k1_10: {
-      return Make_D2Win_DisableAutoMinimizePatch_1_10();
-    }
-
-    case d2::GameVersion::k1_12A: {
-      return Make_D2Win_DisableAutoMinimizePatch_1_12A();
-    }
-
-  }
-}
-
-} // namespace sgd2fdmn::patches
+#endif // SGD2FDMN_PATCHES_REQUIRED_D2WIN_DISABLE_AUTO_MINIMIZE_PATCH_D2WIN_DISABLE_AUTO_MINIMIZE_1_12A_HPP_
